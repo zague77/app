d4f1db2858bbd74c36f47ab67ac516ce7f4dee73
zague77 <alex.j.alves@gmail.com>
2021-07-09 11:21:31 +0100

    graph 1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Values } from './components/Values/Values';
+import * as RGUI from 'react-graphical-ui';
+import {Histogram} from 'react-graphical-ui';
+
+// imp as snippet et
+
 interface Props { }
 interface State {
   values: Array<number>
@@ -9,24 +14,27 @@ export default class App extends React.Component<Props, {}> {
   state: State
   constructor(props: Props) {
     super(props)
-    this.state = { values: [15,12,80,7,70,20] }
+    this.state = { values: [15, 12, 80, 7, 70, 20] }
   }
   render() {
     return <div className="App">
       <Values values={this.state.values}
         addValue={(index: number) => {
-          console.log('add value'+index);
+          console.log('add value' + index);
 
           // mise à jour des données
-          this.setState({values:[...this.state.values,index]})
+          this.setState({ values: [...this.state.values, index] })
         }}
         deleteValue={(index: number) => {
           console.log('delete value');
         }}
       />
-      <hr/>
+      <hr />
+      <RGUI.CloudPoints values={this.state.values}> </RGUI.CloudPoints>
 
-      <hr/>
+      {/*  <Histogram></Histogram> */}
+
+      <hr />
       {JSON.stringify(this.state)}
     </div>;
   }
